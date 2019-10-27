8fc3aec7f55f56a057fee6ded50b6d2b547f3d6e
s1rens <siraj.lyons@gmail.com>
2019-10-27 09:32:14 +0000

    This is a new commit for what I originally planned to be amended

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform Ball;
+    public Vector3 Offset;
+
+    void Start()
+    {
+        transform.LookAt(Ball);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Ball.position + Offset;
+        transform.LookAt(Ball);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    private bool canJump = false;
+    private float JumpSpeed = 10f;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        float j = Input.GetAxisRaw("Jump");
+        Vector3 f = new Vector3(h, 0, v);
+        f *= speed;
+        rb.AddForce(f);
+
+        if (j > 0.2f && canJump)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, j * JumpSpeed, rb.velocity.z);
+            canJump = false;
+        }
+    }
+
+     void OnCollisionEnter(Collision collision)
+     {
+        if (collision.gameObject.tag == "Floor")
+        {
+            canJump = true;
+        }
+    }
+}
